76780f4b6b0e2659f613bcff91bcc567f4cb9554
arulanbarasan <anbu91943@gmail.com>
2025-03-11 11:30:33 +0530

    First commit

---
+++ src/com/MainApp.java
@@ -0,0 +1,14 @@
+package com;
+
+import com.model.Book;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Book book = new Book();
+		book.setTitle("Psychology of money");
+		book.setAuthor("Morgon Housel");
+		System.out.println(book);
+	}
+}
